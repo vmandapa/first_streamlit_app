8f98eaa9fc30988414f61fd15b12dbf28538097e
Varma Mandapati <75353676+vmandapa@users.noreply.github.com>
2022-05-04 14:52:00 -0400

    Update streamline_app.py
    Added Breakfast menu!!

--- streamline_app.py
+++ streamline_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents New Health Diner')
+streamlit.title('My Mom\'s New Health Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.test('Omega 3 & Blueberry Oatmeal')
+streamlit.test('Kale, Spinach & Rocket Smoothie')
+streamit.test('Hard-Boiled FreeRange Egg')
