8aeeda48104411723080f1b458b9c7678f8d14f6
Varma Mandapati <75353676+vmandapa@users.noreply.github.com>
2022-05-05 10:48:57 -0400

    Update streamline_app.py
    added emojis

--- streamline_app.py
+++ streamline_app.py
@@ -2,9 +2,14 @@ import streamlit
 
 streamlit.title('My Mom\'s New Health Diner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled FreeRange Egg')
-streamlit.text('Avocado Toast')
-streamlit.header('Build Your Own Fruit Smoothie')
+streamlit.header('Breakfast Favorites')
+
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
