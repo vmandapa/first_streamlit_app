e5df0863e5a1439552a64ee1f5f2210ef8ad5ec4
Varma Mandapati <75353676+vmandapa@users.noreply.github.com>
2022-05-15 19:00:18 -0400

    Update streamline_app.py

--- streamline_app.py
+++ streamline_app.py
@@ -26,18 +26,24 @@ streamlit.dataframe(fruits_to_show)
 
 # Let's put a pick list here so they can pick the fruit they want included
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
+
+#New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Apple')
-streamlit.write('The user entered', fruit_choice)
+
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
 
 #New Section to display fruityvice api response
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "apple")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-# take the json version of the response and normalize it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
